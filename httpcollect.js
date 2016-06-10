7c828f3a3a3d052d3854fecacf975638974fe3f7
Michael Fossel <mikefossel@gmail.com>
2016-06-09 19:47:02 -0700

    Update httpcollect.js

--- httpcollect.js
+++ httpcollect.js
@@ -1,14 +1,17 @@
 var http = require('http')
+
 var concat = require('concat-stream')
 
 http.get(process.argv[2], function (response) {
 
-response.pipe(concat(function(data){
+    response.pipe(concat(function(data){
+    
+    data = data.toString()
+    
+    console.log(data.length)
+    
+    console.log(data)
 
-data = data.toString()
-console.log(data.length)
-console.log(data)
-
-}))
+      }))
 
 })
